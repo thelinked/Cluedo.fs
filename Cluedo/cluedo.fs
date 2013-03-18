e3ffb9a684bfce2fb395a6c8db52eb27ccc5efab
Daniel Hartnett <thelinked@gmail.com>
2013-03-18 23:56:49 +0000

    Corrected indentation in dummy graphs

--- Cluedo/cluedo.fs
+++ Cluedo/cluedo.fs
@@ -137,20 +137,20 @@
         type 'a Graph = 'a list * 'a Edge list
 
         let g: char Graph = 
-            (['b';'c';'d';'f';'g';'h';'k'], 
-            [('b','c');
-             ('b','f');
-             ('c','f');
-             ('f','k');
-             ('g','h')])  
+                (['b';'c';'d';'f';'g';'h';'k'], 
+                 [('b','c');
+                  ('b','f');
+                  ('c','f');
+                  ('f','k');
+                  ('g','h')])  
 
         type 'a Node = 'a * 'a list
         type 'a AdjacencyGraph = 'a Node list
 
         let ga: char AdjacencyGraph = 
             [('b',['c'; 'f']); 
-             ('c',['b'; 'f']); 
-             ('d',[]); 
-             ('f',['b'; 'c'; 'k']); 
-             ('g',['h']); 
-             ('h',['g']); ('k',['f'])]
+            ('c',['b'; 'f']); 
+            ('d',[]); 
+            ('f',['b'; 'c'; 'k']); 
+            ('g',['h']); 
+            ('h',['g']); ('k',['f'])]
